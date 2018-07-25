07dde213601fb4300338bb502af392ddab9f7e93
Fuzz <fuzz@bluebank.io>
2018-07-25 21:06:52 +0100

    clock

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Clock></Clock>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clock from './components/Clock.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Clock
   }
 }
 </script>
---
+++ src/components/Clock.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="clock">
+    {{ time }}
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'Clock',
+  data: function() {
+    return {
+      time: moment().format('LTS')
+    }
+  },
+  mounted: function() {
+    setInterval(()=>{
+			this.time = moment().format('LTS')
+		},1000)
+  }
+}
+</script>
+
+<style>
+.clock {
+  font-size: 40px;
+}
+</style>
